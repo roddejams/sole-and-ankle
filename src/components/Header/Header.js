53aed95e2f49440ce06b8da9b9d549693beec213
James <james.rodden@savanta.com>
2021-11-11 15:48:07 +0000

    Exercise 2 solution

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -27,13 +27,21 @@ const Header = () => {
 };
 
 const MainHeader = styled.div`
-  padding: 0 32px;
+  padding: 16px 32px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+  display: flex;
+  align-items: baseline;
 `;
 
-const Nav = styled.nav``;
+const Nav = styled.nav`
+  display: flex;
+  gap: 32px;
+  margin-left: auto;
+  margin-right: auto;
+`;
 
 const NavLink = styled.a`
+
   font-size: 1.125rem;
   text-transform: uppercase;
   text-decoration: none;
